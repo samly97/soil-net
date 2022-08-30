c29f23b497a4f08d9a06722a09fc65bf295a1304
samly97 <97.sam.ly@gmail.com>
2022-08-30 12:36:48 -0400

    MUS-7 MUS-11 Add `predict_tortuosity` method to a subclassed tf.keras.Model
    Note that the Finite Difference approximation is of second order accuracy.
    This could be problematic in the case that the stencil operates on solid
    phase instead of the supposed void (concentration) phase.

--- tfcustom/models.py
+++ tfcustom/models.py
@@ -1,7 +1,58 @@
 import tensorflow as tf
+from findiff import FinDiff
 
 
 class SoilNet(tf.keras.Model):
+
+    def predict_tortuosity(
+        self,
+        soil: tf.types.experimental.TensorLike,
+        skel: tf.types.experimental.TensorLike,
+        C_in: float = 1.0,
+        C_out: float = 0.5,
+        dim: int = 128,
+    ):
+        r""" `predict_tortuosity` takes the inputs to the Neural Network and
+        estimates the tortuosity from the predicted concentration map.
+
+        The user could specify the inlet and outlet concentrations as well as
+        the length of the voxel here. Here, it is assumed that the image is
+        cubic.
+        """
+
+        # dC, L, A are scalars - used for effective diffusivity later on
+        dC = C_in - C_out
+        L = dim
+        A = dim ** 3
+
+        # First derivative in the x-axis with O(h^2) accuracy
+        # -- Apparently FinDiff doesn't do odd integer accuracy
+        d_dx = FinDiff(0, 1, 1, acc=2)
+
+        # Predict concentration map and estimate derivative
+        conc_map = self.predict((soil, skel))
+        deriv = tf.numpy_function(d_dx, [conc_map], tf.float32)
+
+        # Gather the inlet derivatives, axis = (batch, x, y, z)
+        inlet_deriv = tf.gather(deriv, 0, axis=1)
+
+        # Reduce twice to get rid of y and z axes
+        flux = tf.math.reduce_sum(inlet_deriv, axis=1)
+        flux = tf.math.reduce_sum(flux, axis=1)
+
+        # Get the effective porosities of the samples
+        eps = tf.math.reduce_sum(soil, axis=1)
+        eps = tf.math.reduce_sum(eps, axis=1)
+        eps = tf.math.reduce_sum(eps, axis=1)
+        eps = tf.math.scalar_mul(1/A, eps)
+
+        # Compute effective diffusivity
+        Deff = tf.math.scalar_mul((L-1)/A/dC, flux)
+
+        # Compute tortuosity
+        tau = tf.math.divide(eps, Deff)
+
+        return tau
 
     def evaluate_abs_rel_err(
         self,
